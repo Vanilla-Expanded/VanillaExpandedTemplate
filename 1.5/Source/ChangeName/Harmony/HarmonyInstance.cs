088d9a895482c5d3f8d0ba75fef2690ccacdb636
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-05-22 08:35:36 +0200

    Initial commit

---
+++ 1.5/Source/ChangeName/Harmony/HarmonyInstance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+
+namespace ChangeName
+{
+
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.ChangeName");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+
+}
